fbb2e4562622932600caeb299634e6a9548a4311
Edward O'Donnell <eddy.odonnell@gmail.com>
2023-11-02 10:03:50 -0400

    Added a method for calculating section count if there are no sections, fixed method

--- src/library/image_manipulation/pipeline_process.py
+++ src/library/image_manipulation/pipeline_process.py
@@ -120,7 +120,7 @@ class Pipeline(
     def get_section_count(self):
         section_count = self.sqlController.get_section_count(self.animal, self.rescan_number)
         if section_count == 0:
-            INPUT = self.fileLocationManager.get_thumbnail
+            INPUT = self.fileLocationManager.thumbnail
             if os.path.exists(INPUT):
                 section_count = len(os.listdir(INPUT))
 
