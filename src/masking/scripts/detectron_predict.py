cb66a9f48d536766d5320089aef086dc0b9ce7ad
Edward O'Donnell <eddy.odonnell@gmail.com>
2024-01-27 13:40:36 +0700

    Added predict for detectron

---
+++ src/masking/scripts/detectron_predict.py
@@ -0,0 +1,60 @@
+from detectron2.utils.logger import setup_logger
+setup_logger()
+
+import os, random, sys
+
+# import some common detectron2 utilities
+from detectron2 import model_zoo
+from detectron2.engine import DefaultPredictor
+from detectron2.config import get_cfg
+from detectron2.utils.visualizer import Visualizer
+from detectron2.utils.visualizer import ColorMode
+
+import torch
+from detectron_test import get_structure_dicts, create_structure_metadata
+import cv2
+
+INPUT = "/net/birdstore/Active_Atlas_Data/data_root/brains_info/masks/structures/detectron/"
+
+"""
+cfg = get_cfg()
+if not torch.cuda.is_available(): 
+    cfg.MODEL.DEVICE = "cpu"
+cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
+cfg.DATASETS.TRAIN = (INPUT + "train",)
+cfg.DATASETS.TEST = ()
+cfg.OUTPUT_DIR = os.path.join(INPUT, 'output')
+cfg.DATALOADER.NUM_WORKERS = 2
+cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")  # Let training initialize from model zoo
+cfg.SOLVER.IMS_PER_BATCH = 2  # This is the real "batch size" commonly known to deep learning people
+cfg.SOLVER.BASE_LR = 0.00025  # pick a good LR
+cfg.SOLVER.MAX_ITER = 300    # 300 iterations seems good enough for this toy dataset; you will need to train longer for a practical dataset
+cfg.SOLVER.STEPS = []        # do not decay learning rate
+cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 128   # The "RoIHead batch size". 128 is faster, and good enough for this toy dataset (default: 512)
+cfg.MODEL.ROI_HEADS.NUM_CLASSES = 1  # only has one class (ballon). 
+"""
+
+cfg = get_cfg()
+if not torch.cuda.is_available(): 
+    cfg.MODEL.DEVICE = "cpu"
+cfg.OUTPUT_DIR = os.path.join(INPUT, 'output')
+cfg.MODEL.WEIGHTS = os.path.join(cfg.OUTPUT_DIR, "model_final.pth")  # path to the model we just trained
+cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.7   # set a custom testing threshold
+predictor = DefaultPredictor(cfg)
+
+structure_metadata = create_structure_metadata()
+i = 1
+dataset_dicts = get_structure_dicts(INPUT + "train")
+for d in random.sample(dataset_dicts, 3):    
+    im = cv2.imread(d["file_name"])
+    outputs = predictor(im)  # format is documented at https://detectron2.readthedocs.io/tutorials/models.html#model-output-format
+    
+    v = Visualizer(im[:, :, ::-1],
+                   metadata=structure_metadata, 
+                   scale=0.5, 
+                   instance_mode=ColorMode.IMAGE_BW   # remove the colors of unsegmented pixels. This option is only available for segmentation models
+    )
+    out = v.draw_instance_predictions(outputs["instances"].to("cpu"))
+    
+    cv2.imwrite(f"/home/eodonnell/tmp/{i}.tif", out.get_image())
+    i += 1
