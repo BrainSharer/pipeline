a4155a9ee3553264abcdfc2cf2a8de9c4fe32dd0
Edward O'Donnell <eddy.odonnell@gmail.com>
2024-02-13 09:58:38 +0700

    Temporarily removed correct multiples in meta manager

--- src/library/image_manipulation/pipeline_process.py
+++ src/library/image_manipulation/pipeline_process.py
@@ -136,7 +136,7 @@ class Pipeline(
     def extract(self):
         print(self.TASK_EXTRACT)
         self.extract_slide_meta_data_and_insert_to_database()
-        self.correct_multiples()
+        #self.correct_multiples()
         self.extract_tiffs_from_czi()
         self.create_web_friendly_image()
         print('Finished extracting.')
