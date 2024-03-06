a9f55b345568d5e90be1a67a7cf00b402cf89ad5
Edward O'Donnell <eddy.odonnell@gmail.com>
2024-03-06 15:42:56 +0700

    Added task to create stitch and it now works with more layers

--- src/library/utilities/brain_stitcher.py
+++ src/library/utilities/brain_stitcher.py
@@ -7,6 +7,7 @@ from scipy.ndimage import zoom
 from tifffile import imwrite
 import math
 from skimage import io
+from pathlib import Path
 
 
 # from library.controller.sql_controller import SqlController
@@ -36,25 +37,38 @@ class BrainStitcher:
             print(f'Error: {self.layer_path} does not exist')
             sys.exit()
         self.debug = debug
+        self.available_layers = [1,2,3]
         self.all_info_files = self.parse_all_info()
         self.scaling_factor = 1/10
-        # self.sqlController = SqlController(self.animal)
+
+    def check_status(self):
+        for layer in self.available_layers:
+            layer = str(layer).zfill(5)
+            infopath = os.path.join(self.base_path, layer, 'info')
+            tifpath = os.path.join(self.base_path, layer, 'tif')
+            infos = sorted(os.listdir(infopath))
+            tifs = sorted(os.listdir(tifpath))
+            print(f'Found {len(infos)} info.json files in layer={layer}')
+            assert len(infos) == len(tifs), "Error, number of tiles does not equal number of json files"
+            for info,tif in zip(infos, tifs):
+                infostem = Path(info).stem
+                tifstem = Path(tif).stem
+                if infostem != tifstem:
+                    print(layer, info, tif)
 
     def parse_all_info(self):
-        available_layers = [1,2]
-        all_info_files = []
-        for layer in available_layers:
+        all_info_files = {}
+        for layer in self.available_layers:
             layer = str(layer).zfill(5)
             infopath = os.path.join(self.base_path, layer, 'info')
             infos = sorted(os.listdir(infopath))
-            print(f'found {len(infos)} info.json files')
             for file in sorted(infos):
                 inpath = os.path.join(infopath, file)
                 with open(inpath) as json_data:
                     d = json.load(json_data)
-                    all_info_files.append(d)
                     json_data.close()
-            print(f'len of all infos={len(all_info_files)}')
+                    infostem = Path(file).stem
+                    all_info_files[(layer, infostem)] = d
 
         return all_info_files
 
@@ -94,31 +108,19 @@ class BrainStitcher:
         if not os.path.exists(infopath):
             print(f'Error: {infopath} does not exist')
             sys.exit()
-        infos = sorted(os.listdir(infopath))
-        print(f'found {len(infos)} info.json files')
-        stitch_tiles = []
-        for file in sorted(infos):
-            inpath = os.path.join(infopath, file)
-            with open(inpath) as json_data:
-                d = json.load(json_data)
-                stitch_tiles.append(d)
-                json_data.close()
-                
-        # set to np arrays
-        TIFPATH = os.path.join(self.layer_path, 'tif')
-        tiles = sorted(os.listdir(TIFPATH))
         # Parameters
         stitch_voxel_size_um = [0.375/self.scaling_factor, 0.375/self.scaling_factor, 1];
-        stack_size_um = self.all_info_files[0]['stack_size_um'];
+        first_element = next(iter(self.all_info_files.values()))
+        stack_size_um = first_element['stack_size_um']
         print('stack_size_um', stack_size_um)
-        #stack_size = stitch_tiles[0]['stack_size'];
         ds_stack_size = [round(stack/stitch) for stack,stitch in zip(stack_size_um, stitch_voxel_size_um)]
-        print('ds_stack_size', ds_stack_size)
+        if self.debug:
+            print('ds_stack_size', ds_stack_size)
 
-        min_z = min(st['layer_z_um'] for st in self.all_info_files)
-        max_z = max(st['layer_z_um'] for st in self.all_info_files)
-        bbox_mmxx_um = np.array([st['tile_mmxx_um'] for st in self.all_info_files])
-        
+        min_z = min(st['layer_z_um'] for st in self.all_info_files.values())
+        max_z = max(st['layer_z_um'] for st in self.all_info_files.values())
+
+        bbox_mmxx_um = np.array([st['tile_mmxx_um'] for st in self.all_info_files.values()])
         vol_bbox_z_mx_um = [min_z, max_z + stack_size_um[2] - 1];
         vol_bbox_mm_um = np.min(bbox_mmxx_um,0)[0:2].tolist()
         vol_bbox_xx_um = np.max(bbox_mmxx_um,0)[2:4].tolist()
@@ -128,36 +130,37 @@ class BrainStitcher:
         vol_bbox_ll_um = [a_i - b_i for a_i, b_i in zip(vol_bbox_xx_um, vol_bbox_mm_um)]
         vol_bbox_ll_um = [a+1 for a in vol_bbox_ll_um]
 
-        print('vol_bbox_ll_um', vol_bbox_ll_um)
+        if self.debug:
+            print('vol_bbox_ll_um', vol_bbox_ll_um)
         ds_bbox_ll = (np.array(vol_bbox_ll_um) / stitch_voxel_size_um)
         ds_bbox_ll = [math.ceil(a) for a in ds_bbox_ll]
         ds_bbox_ll[2] = 250
         b = ds_bbox_ll
         ds_bbox_ll = [b[2], b[0], b[1]]
-        print('ds_bbox_ll', ds_bbox_ll)
-
-        assert len(infos) == len(tiles), "Error, number of tiles does not equal number of json files"
+    
         tmp_stitch_data = np.zeros(ds_bbox_ll, dtype=np.uint16)
         print(f'Big box shape={tmp_stitch_data.shape}')
 
-
-
-        for info,tile in zip(stitch_tiles, tiles):
-            tifpath = os.path.join(TIFPATH, tile)
+        for (layer, position), info in self.all_info_files.items():
+            tile = f"{position}.tif"
+            tifpath = os.path.join(self.base_path, layer, 'tif', tile)
+            if not os.path.exists(tifpath):
+                print(f'Error: missing {tifpath}')
+                sys.exit()
             tif = io.imread(tifpath)            
             tmp_tile_bbox_mm_um = info['tile_mmxx_um'][:2]
             tmp_tile_bbox_mm_um.append(info['layer_z_um'])
             tmp_tile_bbox_ll_um = info['tile_mmll_um'][2:]
             tmp_tile_bbox_ll_um.append(info['stack_size_um'][2])
             tmp_tile_ll_ds_pxl = [round(bbox/voxel) for bbox,voxel in zip(tmp_tile_bbox_ll_um, stitch_voxel_size_um)]
-            print(f'TIF shape= {tif.shape}', end="\t" )
+            print(f'{layer} {position} shape= {tif.shape}', end="\t" )
             print('bounding box', tmp_tile_ll_ds_pxl, end="\t")
-            
-            # Local bounding box 
+
+            # Local bounding box
             tmp_local_bbox_um = [a_i - b_i for a_i, b_i in zip(tmp_tile_bbox_mm_um, vol_bbox_mm_um)]
-            #tmp_local_bbox_mm_ds_pxl = round(tmp_local_bbox_um ./ stitch_voxel_size_um);
+            # tmp_local_bbox_mm_ds_pxl = round(tmp_local_bbox_um ./ stitch_voxel_size_um);
             tmp_local_bbox_mm_ds_pxl = [round(a/b) for a,b in zip(tmp_local_bbox_um, stitch_voxel_size_um)]
-            #print(tmp_local_bbox_mm_ds_pxl)
+            # print(tmp_local_bbox_mm_ds_pxl)
             start_row = tmp_local_bbox_mm_ds_pxl[0]
             start_col = tmp_local_bbox_mm_ds_pxl[1]
             end_row = tif.shape[1] + start_row
@@ -165,9 +168,9 @@ class BrainStitcher:
             print(start_row, end_row, start_col, end_col)
             tmp_stitch_data[0:, start_row:end_row, start_col:end_col] += tif
 
-        #save
-        outpath = os.path.join(self.registration_path, f'{self.layer}.tif')
+        # save
+        outfile = 'layers.' +  '.'.join(map(str, self.available_layers)) + '.tif'
+        outpath = os.path.join(self.registration_path, outfile)
         io.imsave(outpath, tmp_stitch_data)
         print(f'dtype={tmp_stitch_data.dtype} shape={tmp_stitch_data.shape}')
         print('saved', outpath)
-
--- src/pipeline/scripts/create_stitched.py
+++ src/pipeline/scripts/create_stitched.py
@@ -13,13 +13,33 @@ if __name__ == "__main__":
     parser.add_argument("--animal", help="Enter the animal", required=False, default="DK20230126-003", type=str)
     parser.add_argument("--debug", help="Enter true or false", required=False, default="false", type=str)
     parser.add_argument("--channel", help="Enter 1, 2, or 4", required=True, type=int)
-    parser.add_argument("--layer", help="Enter layer", required=True, type=int)
+    parser.add_argument("--layer", help="Enter layer", required=False, default=1, type=int)
+    parser.add_argument(
+        "--task",
+        help="Enter the task you want to perform: \
+                        extract|mask|clean|histogram|align|extra_channel|neuroglancer|status|cell_labels",
+        required=False,
+        default="status",
+        type=str,
+    )
     args = parser.parse_args()
     animal = args.animal
     channel = args.channel
     layer = args.layer
+    task = str(args.task).strip().lower()
     debug = bool({"true": True, "false": False}[str(args.debug).lower()])
 
     pipeline = BrainStitcher(animal, layer, channel, debug=False)
-    pipeline.create_channel_volume_from_h5()
-    #pipeline.stitch_tile()
+
+    function_mapping = {
+        "extract": pipeline.create_channel_volume_from_h5,
+        "stitch": pipeline.stitch_tile,
+        "status": pipeline.check_status,
+    }
+
+    if task in function_mapping:
+        function_mapping[task]()
+    else:
+        print(f"{task} is not a correct task. Choose one of these:")
+        for key in function_mapping.keys():
+            print(f"\t{key}")
