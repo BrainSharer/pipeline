660c40e70f70333f29c53a80f73aa176ea174266
Edward O'Donnell <eddy.odonnell@gmail.com>
2024-03-15 16:52:00 +0700

    Added path to jp2 converter fix 2

--- src/stitching/convert.jp2.tif.m
+++ src/stitching/convert.jp2.tif.m
@@ -1,12 +1,7 @@
-clc;clear;close all;
-% cd('/home/eodonnell/programming/pipeline/src/stitching');
-
-% nohup matlab -nodisplay -nosplash -r "run('convert.jp2.tif.m'); exit" > output.log 2>&1 &
-% Specify the folder where the files live.
 INPUT = '/net/birdstore/Active_Atlas_Data/data_root/pipeline_data/MD590/jp2';
 OUTPUT = '/net/birdstore/Active_Atlas_Data/data_root/pipeline_data/MD590/tif';
 % Check to make sure that folder actually exists.  Warn user if it doesn't.
-if ~isdir(INPUT)
+if ~isfolder(INPUT)
   fprintf('Directory missing %.s\n', INPUT);
   return;
 end
@@ -16,10 +11,9 @@ theFiles = dir(filePattern);
 for k = 1 : length(theFiles)
   baseFileName = theFiles(k).name;
   fullFileName = fullfile(INPUT, baseFileName);
-  [folder, basename, extension] = fileparts(fullFileName);
+  [~, basename, ~] = fileparts(fullFileName);
   img = imread(fullFileName);
   filepath = fullfile(OUTPUT, strcat(basename,'.tif'));
   fprintf(1, 'Writing to %s\n', filepath);  
   imwrite(img, filepath );
 end
-exit;
