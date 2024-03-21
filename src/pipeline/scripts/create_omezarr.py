9048a40ffa1b26334ae18290d6b3a105bb3033c7
Edward O'Donnell <eddy.odonnell@gmail.com>
2024-03-21 15:45:32 +0700

    Added chunk variables and downsample or full resolution to create omezarr

--- src/pipeline/scripts/create_omezarr.py
+++ src/pipeline/scripts/create_omezarr.py
@@ -40,16 +40,29 @@ def get_transformations(axis_names, resolution, n_levels) -> tuple[dict,dict]:
     return transformations
 
 
-
-def create_omezarr(animal, debug):
+def create_omezarr(animal, downsample, debug):
     sqlController = SqlController(animal)
     fileLocationManager = FileLocationManager(animal)
     xy_resolution = sqlController.scan_run.resolution
     z_resolution = sqlController.scan_run.zresolution
-    INPUT = os.path.join(fileLocationManager.prep, 'C1', 'full_aligned')
+    if downsample:
+        scaling_factor = SCALING_FACTOR
+        chunk_factor = 2
+        INPUT = os.path.join(fileLocationManager.prep, 'C1', 'thumbnail_aligned')
+        mips = 4
+    else:
+        scaling_factor = 1
+        chunk_factor = 1
+        INPUT = os.path.join(fileLocationManager.prep, 'C1', 'full_aligned')
+        mips = 7
     if not os.path.exists(INPUT):
         print(f'Missing: {INPUT}')
         sys.exit()
+    files = os.listdir(INPUT)
+    if len(files) < 5:
+        print(f'Not enough files in: {INPUT}')
+        sys.exit()
+
 
     axes = [{'name': 'x', 'type': 'space', 'unit': 'micrometer'},
             {'name': 'y', 'type': 'space', 'unit': 'micrometer'},
@@ -62,50 +75,57 @@ def create_omezarr(animal, debug):
     def mean_dtype(arr, **kwargs):
         return np.mean(arr, **kwargs).astype(arr.dtype)
 
-    stacked = stacked.rechunk((128, 128, 64))
-    downsampled = [stacked]
+    chunk_dict = {
+        1: [128//chunk_factor, 128//chunk_factor, 64//chunk_factor],
+        2: [64//chunk_factor, 64//chunk_factor, 64//chunk_factor],
+        3: [64//chunk_factor, 64//chunk_factor, 64//chunk_factor],
+        4: [64//chunk_factor, 64//chunk_factor, 64//chunk_factor],
+        5: [32//chunk_factor, 32//chunk_factor, 32//chunk_factor],
+        6: [32//chunk_factor, 32//chunk_factor, 32//chunk_factor],
+        7: [32//chunk_factor, 32//chunk_factor, 32//chunk_factor],
+    }
+
+    stacked = stacked.rechunk((128//chunk_factor, 128//chunk_factor, 64))
+    downsampled_stack = [stacked]
     start_time = timer()
-    for mip in (range(1,7)):
-        chunk = int(64 // mip)
-        axis_dict = {0:2,1:2, 2:2}
-        print(f'scaled {mip} chunk={chunk} axis_dict={axis_dict}')
-        scaled = da.coarsen(mean_dtype, downsampled[-1], axis_dict, trim_excess=True).rechunk((chunk, chunk, chunk))
-        downsampled.append(scaled)
+    for mip in (range(1, mips)):
+        chunks = chunk_dict[mip]
+        axis_dict = {0:2, 1:2, 2:2}
+        print(f'scaled {mip} chunks={chunks} axis_dict={axis_dict}')
+        scaled = da.coarsen(mean_dtype, downsampled_stack[-1], axis_dict, trim_excess=True).rechunk(chunks)
+        downsampled_stack.append(scaled)
     end_time = timer()
     elapsed_time = round((end_time - start_time), 2)
     print(f'Creating downsampled mip took {elapsed_time} seconds')
 
-
-    n_levels = len(downsampled)
-    resolution = {'x': xy_resolution*SCALING_FACTOR, 'y': xy_resolution*SCALING_FACTOR, 'z': z_resolution}
+    n_levels = len(downsampled_stack)
+    resolution = {'x': xy_resolution*scaling_factor, 'y': xy_resolution*scaling_factor, 'z': z_resolution}
     transformations = get_transformations(axis_names, resolution, n_levels)
-    print(f'transformations={transformations}')
+    if debug:
+        print(f'transformations={transformations}')
 
     # Open the zarr group manually
     storefile = 'C1.zarr'
     storepath = os.path.join(fileLocationManager.www, 'neuroglancer_data', storefile)
     store = zarr.NestedDirectoryStore(storepath)
     root = zarr.group(store=store, overwrite=True)
-    print(type(root))
     root.attrs['omero'] = {}
-
-    print(root.info)
-    print(transformations)
+    if debug:
+        print(root.info)
 
     # Use OME write multiscale; this actually computes the dask arrays but does so
     # in a memory-efficient way.
-    write_multiscale(downsampled, group=root, axes=axes, coordinate_transformations=transformations)
-
-
+    write_multiscale(downsampled_stack, group=root, axes=axes, coordinate_transformations=transformations)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Work on Animal')
     parser.add_argument('--animal', help='Enter the animal', required=True)
     parser.add_argument("--debug", help="debug", required=False, default=False)
+    parser.add_argument("--downsample", help="downsample", required=False, default=True)
     args = parser.parse_args()
     animal = args.animal
+    downsample = bool({"true": True, "false": False}[str(args.downsample).lower()])
     debug = bool({"true": True, "false": False}[str(args.debug).lower()])
     
-    create_omezarr(animal, debug)
-
+    create_omezarr(animal, downsample, debug)
