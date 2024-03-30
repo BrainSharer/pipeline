44f995bffb27858cdd8da9fc0c2070576e97d640
Edward O'Donnell <eddy.odonnell@gmail.com>
2024-03-30 13:37:59 +0700

    Upped shape in create mesh

--- src/pipeline/scripts/create_neuroglancer_mesh.py
+++ src/pipeline/scripts/create_neuroglancer_mesh.py
@@ -170,7 +170,7 @@ def create_mesh(animal, limit, scaling_factor, skeleton, sharded=True, debug=Fal
         # removing shape results in no 0.shard being created!!!
         # at scale=5, shape=128 did not work but 128*2 did
 
-        s = int(chunk*2)
+        s = int(chunk*3)
         shape = [s, s, s]
         print(f'Creating mesh with shape={shape} at mip={mesh_mip} with shards={str(sharded)}')
         tasks = tc.create_meshing_tasks(layer_path, mip=mesh_mip, shape=shape, compress=True, sharded=sharded, max_simplification_error=max_simplification_error) # The first phase of creating mesh
