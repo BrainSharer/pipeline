33374ad307522ff6b02abfc78d4e43ee93f83deb
Edward O'Donnell <eddy.odonnell@gmail.com>
2024-05-04 07:33:29 +0700

    Put in fix for odd sized images

--- src/library/image_manipulation/image_manager.py
+++ src/library/image_manipulation/image_manager.py
@@ -3,24 +3,25 @@ import numpy as np
 import tifffile
 
 class ImageManager:
-    def __init__(self, directory, maskpath=None):
+    def __init__(self, directory):
         self.files = sorted(os.listdir(directory))
-        self.masks = sorted(os.listdir(maskpath))
         len_files = len(self.files)
-        assert len_files == len(self.masks), 'Number of files and masks do not match.'
-        midpoint = len_files // 2
-        midfile = self.files[midpoint]
-        midmaskfile = self.masks[midpoint]
+        self.midpoint = len_files // 2
+        midfile = self.files[self.midpoint]
         midfilepath = os.path.join(directory, midfile)
-        midmaskpath = os.path.join(maskpath, midmaskfile)
         self.img = tifffile.imread(midfilepath)
         self.dtype = self.img.dtype
         self.ndim = self.img.ndim
         self.shape = self.img.shape
+
+
+
+    def get_bgcolor(self, maskpath):
+        self.masks = sorted(os.listdir(maskpath))
+        midmaskfile = self.masks[self.midpoint]
+        midmaskpath = os.path.join(maskpath, midmaskfile)
+
         self.mask = tifffile.imread(midmaskpath)
-
-
-    def get_bgcolor(self):
 
         if self.mask is None:
             print('Warning: no mask is availabe for this image')
