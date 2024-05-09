227389192e0ceadcd5f2c2a737a2753e7fc136ee
Edward O'Donnell <eddy.odonnell@gmail.com>
2024-05-09 17:01:43 +0700

    Updated initial chunks

--- src/library/image_manipulation/omezarr_manager.py
+++ src/library/image_manipulation/omezarr_manager.py
@@ -234,7 +234,7 @@ class OmeZarrManager():
             old_shape = tiff_stack.shape
             new_shape = aligned_coarse_chunks(old_shape[:3], 64)
             tiff_stack = tiff_stack[:, 0:new_shape[1], 0:new_shape[2]]
-            tiff_stack = tiff_stack.rechunk('auto')      
+            tiff_stack = tiff_stack.rechunk([1, new_shape[1]//2, new_shape[2]//2])      
         print(f'tiff_stack shape={tiff_stack.shape} tiff_stack.chunksize={tiff_stack.chunksize} stored chunks={tiff_stack.chunksize}')
         z = zarr.zeros(tiff_stack.shape, chunks=tiff_stack.chunksize, store=store, overwrite=True, dtype=self.dtype)
         if client is None:
