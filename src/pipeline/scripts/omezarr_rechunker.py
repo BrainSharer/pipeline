e9d8fa2f270e5a89607fe1503bfe4cc773e0d81d
Edward O'Donnell <eddy.odonnell@gmail.com>
2024-05-20 08:47:52 +0700

    Added rechunker back in to omezarr rechunker

--- src/pipeline/scripts/omezarr_rechunker.py
+++ src/pipeline/scripts/omezarr_rechunker.py
@@ -4,6 +4,7 @@ import sys
 from pathlib import Path
 from timeit import default_timer as timer
 
+import psutil
 import zarr
 import dask
 import dask.array as da
@@ -11,6 +12,16 @@ import numpy as np
 from timeit import default_timer as timer
 from distributed import Client, progress
 from dask.diagnostics import ProgressBar
+from pathlib import Path
+import sys
+import os
+import zarr
+from dask.distributed import Client
+import argparse
+import numpy as np
+import dask.array as da
+from timeit import default_timer as timer
+from rechunker import rechunk
 
 PIPELINE_ROOT = Path("./src").absolute()
 sys.path.append(PIPELINE_ROOT.as_posix())
@@ -37,6 +48,10 @@ class OmezarrManager():
         self.channel = channel
         self.downsample = downsample
         self.debug = debug
+        tmp_dir = get_scratch_dir()
+        self.tmp_dir = os.path.join(tmp_dir, f'{self.animal}')
+        os.makedirs(self.tmp_dir, exist_ok=True)
+
         self.fileLocationManager = FileLocationManager(animal)
         if self.downsample:
             self.storefile = 'C1T.zarr'
@@ -59,61 +74,93 @@ class OmezarrManager():
 
     def info(self):
 
-        if os.path.exists(os.path.join(self.storepath, 'scale0')):
-            print(f'Using existing {self.storepath}')
-        else:
-            print(f'No zarr: {self.storepath}')
-            return
-        
-        store = get_store(self.storepath, 0, 'r')
-        volume = zarr.open(store, 'r')
-        print(volume.info)
-        print(f'volume.shape={volume.shape}')
+        paths = [self.storepath, self.rechunkmepath]
+        for path in paths:
+            if os.path.exists(path):
+                print(f'Using existing {path}')   
+                store = get_store(path, 0, 'r')
+                volume = zarr.open(store, 'r')
+                print(volume.info)
+                print(f'volume.shape={volume.shape}')
+
+    @staticmethod
+    def create_target_chunks(shape):
+        """Create target chunks based on the shape of the array.
+
+        Args:
+            shape (tuple): Shape of the array.
+
+        Returns:
+            tuple: Target chunks.
+        """
+        # Define your logic to create target chunks here
+        rows = shape[-2] // 4
+        columns = shape[-1] // 4
+
+        target_chunks = (1, 1, 1, rows, columns)
+        return target_chunks
+
 
     def rechunkme(self):
-        with Client(n_workers=2, threads_per_worker=4) as client:
     
-            if os.path.exists(os.path.join(self.storepath, 'scale0')):
-                print('Rechunked store exists, no need to rechunk full resolution.\n')
-                return
+        if os.path.exists(os.path.join(self.storepath, 'scale0')):
+            print('Rechunked store exists, no need to rechunk full resolution.\n')
+            return
 
-            read_storepath = os.path.join(self.rechunkmepath, 'scale0')
-            write_storepath = os.path.join(self.storepath, 'scale0')
-            print(f'Loading data at: {read_storepath}', end=" ")
-            if os.path.exists(read_storepath):
-                print(': Success!')
-            else:
-                print('\nError: exiting ...')
-                print(f'Missing {read_storepath}')            
-                sys.exit()
+        read_storepath = os.path.join(self.rechunkmepath, 'scale0')
+        write_storepath = os.path.join(self.storepath, 'scale0')
+        print(f'Loading data at: {read_storepath}', end=" ")
+        if os.path.exists(read_storepath):
+            print(': Success!')
+        else:
+            print('\nError: exiting ...')
+            print(f'Missing {read_storepath}')            
+            sys.exit()
 
-            if os.path.exists(write_storepath):
-                print(f'Already exists: {write_storepath}')            
-                return
+        
+        if os.path.exists(write_storepath):
+            print(f'Already exists: {write_storepath}')            
+            return
 
-            rechunkme_stack = da.from_zarr(url=read_storepath)
-            print(f'Using rechunking store with shape={rechunkme_stack.shape} chunks={rechunkme_stack.chunksize}')
-            leading_chunk = rechunkme_stack.shape[2]
-            target_chunks = (1, 1, 1, rechunkme_stack.shape[1]//4, rechunkme_stack.shape[2]//4)
-            if leading_chunk < target_chunks[2]:
-                target_chunks = (1, 1, leading_chunk, rechunkme_stack.shape[1]//4, rechunkme_stack.shape[2]//4)
-            start_time = timer()
-            rechunked = rechunkme_stack.rechunk(target_chunks)
-            end_time = timer()
-            total_elapsed_time = round((end_time - start_time), 2)
-            print(f'Rechunking to chunk={rechunked.chunksize} took {total_elapsed_time} seconds.')
-            del rechunkme_stack 
-            store = get_store(self.storepath, 0)
-            z = zarr.zeros(rechunked.shape, chunks=target_chunks, store=store, overwrite=True, dtype=self.dtype)
+        rechunkme_stack = da.from_zarr(url=read_storepath)
+        #target_chunks = self.create_target_chunks(rechunkme_stack.shape)
+        print(f'Using existing store with old shape={rechunkme_stack.shape} chunks={rechunkme_stack.chunksize}', end=" ")
+        GB = (psutil.virtual_memory().free // 1024**3) * 0.8
+        max_mem = f"{GB}GB"        
+        start_time = timer()
+        rechunkme_stack = rechunkme_stack.rechunk('auto')
+        rechunkme_stack = rechunkme_stack.reshape(1, 1, *rechunkme_stack.shape)
+        rechunkme_stack = rechunkme_stack.rechunk('auto')
+        print(f'New shape={rechunkme_stack.shape} new chunks={rechunkme_stack.chunksize}')
+        end_time = timer()
+        total_elapsed_time = round((end_time - start_time), 2)
+        print(f'Rechunking to chunk={rechunkme_stack.chunksize} took {total_elapsed_time} seconds.')
+        
+        store = get_store(self.storepath, 0)
+        temp_store = os.path.join(self.tmp_dir, "rechunked-tmp.zarr")
+        array_plan = rechunk(
+            rechunkme_stack, rechunkme_stack.chunksize, max_mem, store, temp_store=temp_store
+        )
+        print(f'Executing plan with mem={max_mem}')
+        start_time = timer()
+        with ProgressBar():
+            rechunked = array_plan.execute()        
+        print('Plan executed')
+        #rechunked = da.from_zarr(rechunked)
+        store = get_store(self.storepath, 0)
+        target_chunks = (1, 1, 1, 2048, 2048)
+        workers = 2
+        jobs = 4
+        z = zarr.zeros(rechunked.shape, chunks=target_chunks, store=store, overwrite=True, dtype=self.dtype)
+        with Client(n_workers=workers, threads_per_worker=jobs) as client:
+            print(f'Writing to zarr with workers={workers} jobs={jobs} target_chunks={target_chunks} dtype={self.dtype}')
             to_store = da.store(rechunked, z, lock=False, compute=False)
-
-            start_time = timer()
-            print(f'Storing type={type(to_store)} rechunked data to: {write_storepath}')
             to_store = progress(client.compute(to_store))
-            end_time = timer()
-            total_elapsed_time = round((end_time - start_time), 2)
-            print(f'Wrote rechunked data to: {write_storepath} took {total_elapsed_time} seconds.')
-            print()
+            to_store = client.gather(to_store)
+
+        end_time = timer()
+        total_elapsed_time = round((end_time - start_time), 2)
+        print(f'Wrote rechunked data to: {write_storepath} took {total_elapsed_time} seconds.')
 
 
 
