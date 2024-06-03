c00bbf1df8cea602984084d5747a53254c15be8a
Edward O'Donnell <eddy.odonnell@gmail.com>
2024-06-03 11:43:55 +0700

    Fixed paths in coco utils

--- src/library/mask_utilities/coco_utils.py
+++ src/library/mask_utilities/coco_utils.py
@@ -3,7 +3,7 @@ import os
 import torch
 import torch.utils.data
 import torchvision
-import transforms as T
+from library.mask_utilities import transforms as T
 from pycocotools import mask as coco_mask
 from pycocotools.coco import COCO
 
