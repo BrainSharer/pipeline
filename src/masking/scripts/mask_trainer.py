ecd2b49b5826898833872a051ac700cab2628cf4
Edward O'Donnell <eddy.odonnell@gmail.com>
2024-06-03 11:32:46 +0700

    Updated torchvision files

--- src/masking/scripts/mask_trainer.py
+++ src/masking/scripts/mask_trainer.py
@@ -18,180 +18,167 @@ from library.image_manipulation.mask_manager import MaskManager
 from library.mask_utilities.utils import collate_fn
 from library.mask_utilities.engine import train_one_epoch, evaluate
 
-ROOT = '/net/birdstore/Active_Atlas_Data/data_root/brains_info/masks'
+class MaskTrainer():
 
-def train_and_test(epochs):
-    # train on the GPU or on the CPU, if a GPU is not available
-    device = get_device()
-
-
-    # our dataset has two classes only - background and person
-    num_classes = 2
-    # use our dataset and defined transformations
-    dataset = MaskDataset(ROOT, animal=None, transforms = get_transform(train=True))
-    dataset_test = MaskDataset(ROOT, animal=None, transforms = get_transform(train=False))
-
-    # split the dataset in train and test set
-    indices = torch.randperm(len(dataset)).tolist()
-    dataset = torch.utils.data.Subset(dataset, indices[:-50])
-    dataset_test = torch.utils.data.Subset(dataset_test, indices[-50:])
-
-    # define training and validation data loaders
-    data_loader = torch.utils.data.DataLoader(
-        dataset,
-        batch_size=2,
-        shuffle=True,
-        num_workers=4,
-        collate_fn=collate_fn
-    )
-
-    data_loader_test = torch.utils.data.DataLoader(
-        dataset_test,
-        batch_size=1,
-        shuffle=False,
-        num_workers=4,
-        collate_fn=collate_fn
-    )
-
-    # get the model using our helper function
-    mask_manager = MaskManager()
-    model = mask_manager.get_model_instance_segmentation(num_classes)
-
-    # move model to the right device
-    model.to(device)
-
-    # construct an optimizer
-    params = [p for p in model.parameters() if p.requires_grad]
-    optimizer = torch.optim.SGD(
-        params,
-        lr=0.005,
-        momentum=0.9,
-        weight_decay=0.0005
-    )
-
-    # and a learning rate scheduler
-    lr_scheduler = torch.optim.lr_scheduler.StepLR(
-        optimizer,
-        step_size=3,
-        gamma=0.1
-    )
-
-    print(f"We have: {len(dataset)} images to train and {len(dataset_test)} images to test over {epochs} epochs.")
-
-    for epoch in range(epochs):
-        # train for one epoch, printing every 10 iterations
-        train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq=10)
-        # update the learning rate
-        lr_scheduler.step()
-        # evaluate on the test dataset
-        evaluate(model, data_loader_test, device=device)
-
-    print("That's it!")    
-
-def train(animal, structures, epochs, num_classes, debug):
-
-    if structures:
-        ROOT = os.path.join(ROOT, 'structures')
-        dataset = StructureDataset(ROOT, transforms = get_transform(train=True))
-    else:
-        dataset = MaskDataset(ROOT, animal, transforms = get_transform(train=True))
-
-    indices = torch.randperm(len(dataset)).tolist()
-
-    if debug:
-        test_cases = 12
-        torch.manual_seed(1)
-        torch_dataset = torch.utils.data.Subset(dataset, indices[0:test_cases])
-    else:
-        torch_dataset = torch.utils.data.Subset(dataset, indices)
-
-    workers = 2
-    batch_size = 4
-    ## the line below is very important for data on an NFS file system!
-    torch.multiprocessing.set_sharing_strategy('file_system')
-
-    device = get_device()
-
-    # define training and validation data loaders
-    data_loader = torch.utils.data.DataLoader(
-        torch_dataset, batch_size=batch_size, shuffle=True, num_workers=workers,
-        collate_fn=collate_fn)
-
-    n_files = len(torch_dataset)
-    print_freq = 10
-    if n_files > 1000:
-        print_freq = 100
-    print(f"We have: {n_files} images to train from {dataset.img_root} and printing loss info every {print_freq} iterations.")
-    # our dataset has two classs, tissue or 'not tissue'
-    modelpath = os.path.join(ROOT, 'mask.model.pth')
-    # create logging file
-    logpath = os.path.join(ROOT, "mask.logger.txt")
-    logfile = open(logpath, "w")
-    logheader = f"Masking {datetime.now()} with {epochs} epochs from {dataset.img_root} with {n_files} files.\n"
-    logfile.write(logheader)
-    # get the model using our helper function
-    mask_manager = MaskManager()
-    model = mask_manager.get_model_instance_segmentation(num_classes)
-    # move model to the right device
-    model.to(device)
-    # construct an optimizer
-    params = [p for p in model.parameters() if p.requires_grad]
-    optimizer = torch.optim.SGD(params, lr=0.005,momentum=0.9, weight_decay=0.0005)
-    # and a learning rate scheduler which decreases the learning rate by # 10x every 3 epochs
-    lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=3, gamma=0.1)
-    loss_list = []
-    
-    # original version with train_one_epoch
-    for epoch in range(epochs):
-        # train for one epoch, printing every 10 iterations
-        mlogger = train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq=print_freq)
-        loss_txt = str(mlogger.loss)
-        x = loss_txt.split()
-        loss = float(x[0])
-        del x
-        loss_mask_txt = str(mlogger.loss_mask)
-        x = loss_mask_txt.split()
-        loss_mask = float(x[0])
-        loss_list.append([loss, loss_mask])
-        # update the learning rate
-        lr_scheduler.step()
-        if not debug:
-            torch.save(model.state_dict(), modelpath)
-
-    logfile.write(str(loss_list))
-    logfile.write("\n")
-    print('Finished with masks')
-    logfile.close()
-    print('Creating loss chart')
-
-    fig = plt.figure()
-    output_path = os.path.join(ROOT, 'loss_plot.png')
-    x = [i for i in range(len(loss_list))]
-    l1 = [i[0] for i in loss_list]
-    l2 = [i[1] for i in loss_list]
-    plt.plot(x, l1,  color='green', linestyle='dashed', marker='o', markerfacecolor='blue', markersize=5, label="Loss")
-    plt.plot(x, l2,  color='red', linestyle=':', marker='o', markerfacecolor='yellow', markersize=5, label="Mask loss")
-    plt.style.use("ggplot")
-    plt.xticks(np.arange(min(x), max(x)+1, 1.0))
-    plt.xlabel('Epochs')
-    plt.ylabel('Loss')
-    plt.title(f'Loss over {len(x)} epochs with {len(dataset)} images')
-    plt.legend()
-    plt.close()
-    fig.savefig(output_path, bbox_inches="tight")
-    print('Finished with loss plot')
+    def __init__(self):
+        self.root = '/net/birdstore/Active_Atlas_Data/data_root/brains_info/masks'
+        self.workers = 2
+        self.batch_size = 4
+        if torch.cuda.is_available(): 
+            self.device = torch.device('cuda') 
+            print('Using Nvidia graphics card GPU.')
+        else:
+            warnings.filterwarnings("ignore")
+            self.device = torch.device('cpu')
+            print('No Nvidia card found, using CPU.')
 
 
-def get_device():
-    if torch.cuda.is_available(): 
-        device = torch.device('cuda') 
-        print(f'Using Nvidia graphics card GPU.')
-    else:
-        warnings.filterwarnings("ignore")
-        device = torch.device('cpu')
-        print(f'No Nvidia card found, using CPU.')
+    def train_and_test(self, epochs):
+        # our dataset has two classes only - background and person
+        num_classes = 2
+        # use our dataset and defined transformations
+        dataset = MaskDataset(self.root, animal=None, transforms = get_transform(train=True))
+        dataset_test = MaskDataset(self.root, animal=None, transforms = get_transform(train=False))
 
-    return device
+        # split the dataset in train and test set
+        indices = torch.randperm(len(dataset)).tolist()
+        dataset = torch.utils.data.Subset(dataset, indices[:-50])
+        dataset_test = torch.utils.data.Subset(dataset_test, indices[-50:])
+
+        # define training and validation data loaders
+        data_loader = torch.utils.data.DataLoader(
+            dataset,
+            batch_size=2,
+            shuffle=True,
+            num_workers=4,
+            collate_fn=collate_fn
+        )
+
+        data_loader_test = torch.utils.data.DataLoader(
+            dataset_test,
+            batch_size=1,
+            shuffle=False,
+            num_workers=4,
+            collate_fn=collate_fn
+        )
+
+        # get the model using our helper function
+        mask_manager = MaskManager()
+        model = mask_manager.get_model_instance_segmentation(num_classes)
+        # move model to the right device
+        model.to(self.device)
+
+        # construct an optimizer
+        params = [p for p in model.parameters() if p.requires_grad]
+        optimizer = torch.optim.SGD(params, lr=0.005,momentum=0.9, weight_decay=0.0005)
+        # and a learning rate scheduler which decreases the learning rate by # 10x every 3 epochs
+        lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=3, gamma=0.1)
+        n_files = len(dataset)
+        print_freq = 10
+        if n_files > 1000:
+            print_freq = 100
+
+        print(f"We have: {len(dataset)} images to train and {len(dataset_test)} images to test over {epochs} epochs.")
+
+        for epoch in range(epochs):
+            train_one_epoch(model, optimizer, data_loader, self.device, epoch, print_freq=print_freq)
+            # update the learning rate
+            lr_scheduler.step()
+            # evaluate on the test dataset
+            evaluate(model, data_loader_test, device=self.device)
+
+        print("That's it!")    
+
+    def train(self, animal, structures, epochs, num_classes, debug):
+
+        if structures:
+            structure_root = os.path.join(self.root, 'structures')
+            dataset = StructureDataset(structure_root, transforms = get_transform(train=True))
+        else:
+            dataset = MaskDataset(self.root, animal, transforms = get_transform(train=True))
+
+        indices = torch.randperm(len(dataset)).tolist()
+
+        if debug:
+            test_cases = 12
+            torch.manual_seed(1)
+            torch_dataset = torch.utils.data.Subset(dataset, indices[0:test_cases])
+        else:
+            torch_dataset = torch.utils.data.Subset(dataset, indices)
+
+        ## the line below is very important for data on an NFS file system!
+        torch.multiprocessing.set_sharing_strategy('file_system')
+
+
+        # define training and validation data loaders
+        data_loader = torch.utils.data.DataLoader(
+            torch_dataset, batch_size=self.batch_size, shuffle=True, num_workers=self.workers,
+            collate_fn=collate_fn)
+
+        n_files = len(torch_dataset)
+        print_freq = 10
+        if n_files > 1000:
+            print_freq = 100
+        print(f"We have: {n_files} images to train from {dataset.img_root} and printing loss info every {print_freq} iterations.")
+        # our dataset has two classs, tissue or 'not tissue'
+        modelpath = os.path.join(self.root, 'mask.model.pth')
+        # create logging file
+        logpath = os.path.join(self.root, "mask.logger.txt")
+        logfile = open(logpath, "w")
+        logheader = f"Masking {datetime.now()} with {epochs} epochs from {dataset.img_root} with {n_files} files.\n"
+        logfile.write(logheader)
+        # get the model using our helper function
+        mask_manager = MaskManager()
+        model = mask_manager.get_model_instance_segmentation(num_classes)
+        # move model to the right device
+        model.to(self.device)
+        # construct an optimizer
+        params = [p for p in model.parameters() if p.requires_grad]
+        optimizer = torch.optim.SGD(params, lr=0.005,momentum=0.9, weight_decay=0.0005)
+        # and a learning rate scheduler which decreases the learning rate by # 10x every 3 epochs
+        lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=3, gamma=0.1)
+        loss_list = []
+        
+        # original version with train_one_epoch
+        for epoch in range(epochs):
+            # train for one epoch, printing every 10 iterations
+            mlogger = train_one_epoch(model, optimizer, data_loader, self.device, epoch, print_freq=print_freq)
+            loss_txt = str(mlogger.loss)
+            x = loss_txt.split()
+            loss = float(x[0])
+            del x
+            loss_mask_txt = str(mlogger.loss_mask)
+            x = loss_mask_txt.split()
+            loss_mask = float(x[0])
+            loss_list.append([loss, loss_mask])
+            # update the learning rate
+            lr_scheduler.step()
+            if not debug:
+                torch.save(model.state_dict(), modelpath)
+
+        logfile.write(str(loss_list))
+        logfile.write("\n")
+        print('Finished with masks')
+        logfile.close()
+        print('Creating loss chart')
+
+        fig = plt.figure()
+        output_path = os.path.join(self.root, 'loss_plot.png')
+        x = [i for i in range(len(loss_list))]
+        l1 = [i[0] for i in loss_list]
+        l2 = [i[1] for i in loss_list]
+        plt.plot(x, l1,  color='green', linestyle='dashed', marker='o', markerfacecolor='blue', markersize=5, label="Loss")
+        plt.plot(x, l2,  color='red', linestyle=':', marker='o', markerfacecolor='yellow', markersize=5, label="Mask loss")
+        plt.style.use("ggplot")
+        plt.xticks(np.arange(min(x), max(x)+1, 1.0))
+        plt.xlabel('Epochs')
+        plt.ylabel('Loss')
+        plt.title(f'Loss over {len(x)} epochs with {len(dataset)} images')
+        plt.legend()
+        plt.close()
+        fig.savefig(output_path, bbox_inches="tight")
+        print('Finished with loss plot')
+
 
 
 if __name__ == '__main__':
@@ -210,10 +197,11 @@ if __name__ == '__main__':
     animal = args.animal
     epochs = args.epochs
     num_classes = args.num_classes
+    mask_trainer = MaskTrainer()
     if test:
-        train_and_test(epochs)
+        mask_trainer.train_and_test(epochs)
     else:
-        train(animal, structures, epochs, num_classes, debug)
+        mask_trainer.train(animal, structures, epochs, num_classes, debug)
 
 
 
