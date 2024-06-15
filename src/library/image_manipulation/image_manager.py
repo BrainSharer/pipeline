5e100a63666c6857a6b16d19610cddc8c267e4cf
Edward O'Donnell <eddy.odonnell@gmail.com>
2024-06-16 06:57:11 +0700

    Cleaned up code and renamed some variables

--- src/library/image_manipulation/image_manager.py
+++ src/library/image_manipulation/image_manager.py
@@ -7,8 +7,8 @@ from skimage import io
 class ImageManager:
     def __init__(self, directory):
         self.files = sorted(os.listdir(directory))
-        len_files = len(self.files)
-        self.midpoint = len_files // 2
+        self.len_files = len(self.files)
+        self.midpoint = self.len_files // 2
         self.midfile = self.files[self.midpoint]
         midfilepath = os.path.join(directory, self.midfile)
         self.img = io.imread(midfilepath)
@@ -18,7 +18,7 @@ class ImageManager:
         self.width = self.shape[1]
         self.height = self.shape[0]
         self.center = np.array([self.width, self.height]) / 2
-        self.volume_size = (self.width, self.height, len_files)
+        self.volume_size = (self.width, self.height, self.len_files)
         self.num_channels = self.img.shape[2] if len(self.img.shape) > 2 else 1
 
 
