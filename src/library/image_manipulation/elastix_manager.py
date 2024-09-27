1dea09803fe9ab9c9ad0e68ff385e9567f90a27a
drinehart1 <duane.rinehart@gmail.com>
2024-09-27 10:07:46 -0700

    ADDED GPU ELASTIX - FASTER, STABLE

--- src/library/image_manipulation/elastix_manager.py
+++ src/library/image_manipulation/elastix_manager.py
@@ -6,6 +6,7 @@ The libraries are contained within the SimpleITK-SimpleElastix library
 
 import math
 import os
+from pathlib import Path
 import numpy as np
 from collections import OrderedDict
 from PIL import Image
@@ -30,6 +31,15 @@ from library.utilities.utilities_registration import (
 )
 from library.image_manipulation.image_manager import ImageManager
 
+#GPU alt TESTING
+import cupy as cp
+
+# import torch
+# import torch.nn as nn
+# import torch.nn.functional as F
+# from torchvision import transforms
+# from torch.optim.optimizer import Optimizer, required
+
 
 class ElastixManager(FileLogger):
     """Class for generating, storing and applying transformations within 
@@ -43,6 +53,7 @@ class ElastixManager(FileLogger):
         self.registration_output = os.path.join(self.fileLocationManager.prep, 'registration')
         self.input, self.output = self.fileLocationManager.get_alignment_directories(channel=self.channel, resolution='thumbnail')
         self.maskpath = self.fileLocationManager.get_thumbnail_masked(channel=1) # usually channel=1, except for step 6
+        
 
     def create_within_stack_transformations(self):
         """Calculate and store the rigid transformation using elastix.  
@@ -51,16 +62,20 @@ class ElastixManager(FileLogger):
         up to an hour to run for a stack. It only needs to be run once for
         each brain. 
         """
-        test_dir(self.animal, self.input, self.section_count, True, same_size=True)
-        files = sorted(os.listdir(self.input))
-        nfiles = len(files)
+        if self.debug:
+            print(f"DEBUG: START ElastixManager::create_within_stack_transformations")
+
+        files, nfiles = test_dir(self.animal, self.input, self.section_count, True, same_size=True)
+        
         self.logevent(f"Input FOLDER: {self.input}")
         self.logevent(f"FILE COUNT: {nfiles}")
+        
         for i in range(1, nfiles):
             fixed_index = os.path.splitext(files[i - 1])[0]
             moving_index = os.path.splitext(files[i])[0]
             if not self.sqlController.check_elastix_row(self.animal, moving_index):
-                rotation, xshift, yshift, metric = self.align_elastix(fixed_index, moving_index, use_points=False)
+                #rotation, xshift, yshift, metric = self.align_elastix(fixed_index, moving_index, use_points=False) #DEPRECATED IN SEP-2024; REMOVE IN 2025 IF NO LONGER NEEDED
+                rotation, xshift, yshift, metric = self.align_images_elastix_GPU(fixed_index, moving_index) #pyCUDA elastix GPU apt (WORKING + FASTER AS OF 20-SEP-2024)
                 self.sqlController.add_elastix_row(self.animal, moving_index, rotation, xshift, yshift, metric)
 
     def update_within_stack_transformations(self):
@@ -114,6 +129,7 @@ class ElastixManager(FileLogger):
 
     def align_elastix(self, fixed_index, moving_index, use_points=False):
         """
+        DEPRECATED IN SEP-2024; REMOVE IN 2025 IF NO LONGER USED
         Aligns two images using the Elastix registration algorithm.
 
         Args:
@@ -179,6 +195,99 @@ class ElastixManager(FileLogger):
 
         return float(R), float(x), float(y), float(metric)
 
+    def align_images_elastix_GPU(self, fixed_index: str, moving_index: str, use_points: bool = False) -> tuple[float, float, float, float]:
+        """
+        Aligns two images using the Elastix registration algorithm with GPU acceleration.
+        expected to replace 'align_elastix' (TESTING)
+
+        Args:
+            fixed_index (str due to filename extraction): The index of the fixed image.
+            moving_index (str due to filename extraction): The index of the moving image.
+            use_points (bool, optional): Whether to use corresponding points for registration. Defaults to False.
+
+        Returns:
+            tuple: A tuple containing the rotation angle (R), translation in the x-axis (x), translation in the y-axis (y),
+            and the registration metric.
+
+        Raises:
+            AssertionError: If the number of fixed points does not match the number of moving points.
+        """
+        # Transfer images to GPU memory
+        def to_gpu(image):
+            array = sitk.GetArrayFromImage(image)
+            gpu_array = cp.asarray(array)
+            return sitk.GetImageFromArray(cp.asnumpy(gpu_array))
+
+        
+        if use_points:
+            fixed_point_file = os.path.join(self.registration_output, f'{fixed_index}_points.txt')
+            moving_point_file = os.path.join(self.registration_output, f'{moving_index}_points.txt')
+            if os.path.exists(fixed_point_file) and os.path.exists(moving_point_file):
+                print(f'Found fixed point file: {os.path.basename(os.path.normpath(fixed_point_file))}', end=" ")
+                print(f'and moving point file: {os.path.basename(os.path.normpath(moving_point_file))}')
+                with open(fixed_point_file, 'r') as fp:
+                    fixed_count = len(fp.readlines())
+                with open(moving_point_file, 'r') as fp:
+                    moving_count = len(fp.readlines())
+                assert fixed_count == moving_count, \
+                        f'Error, the number of fixed points in {fixed_point_file} do not match {moving_point_file}'
+            else:
+                return 0, 0, 0, 0
+
+        # Use CUDA-enabled SimpleITK
+        #sitk.ProcessObject.SetGlobalDefaultNumberOfThreads(16) #maybe host-dependent?
+
+        if self.debug:
+            sitk.ProcessObject.SetGlobalDefaultDebug(True)
+
+        elastixImageFilter = sitk.ElastixImageFilter()
+        fixed_file = os.path.join(self.input, f"{fixed_index}.tif")
+        fixed = sitk.ReadImage(fixed_file, self.pixelType)
+
+        moving_file = os.path.join(self.input, f"{moving_index}.tif")
+        moving = sitk.ReadImage(moving_file, self.pixelType)
+
+        fixed_gpu = to_gpu(fixed)
+        moving_gpu = to_gpu(moving)
+
+        # Set the images in the filter
+        elastixImageFilter.SetFixedImage(fixed_gpu)
+        elastixImageFilter.SetMovingImage(moving_gpu)
+
+        rigid_params = create_rigid_parameters(elastixImageFilter, debug=self.debug)
+        elastixImageFilter.SetParameterMap(rigid_params)
+
+        if use_points:
+            elastixImageFilter.SetParameter("Registration", ["MultiMetricMultiResolutionRegistration"])
+            elastixImageFilter.SetParameter("Metric",  ["AdvancedMattesMutualInformation", "CorrespondingPointsEuclideanDistanceMetric"])
+            elastixImageFilter.SetParameter("Metric0Weight", ["0.25"]) # the weight of 1st metric for each resolution
+            elastixImageFilter.SetParameter("Metric1Weight",  ["0.75"]) # the weight of 2nd metric
+            elastixImageFilter.SetFixedPointSetFileName(fixed_point_file)
+            elastixImageFilter.SetMovingPointSetFileName(moving_point_file)
+
+        elastixImageFilter.SetLogToFile(True)
+        logpath = os.path.join(self.registration_output, 'iteration_logs')
+        os.makedirs(logpath, exist_ok=True)
+        elastixImageFilter.SetOutputDirectory(logpath)        
+
+        elastixImageFilter.LogToConsoleOff()
+        if self.debug:
+            elastixImageFilter.PrintParameterMap()
+        
+        # Execute the registration on GPU
+        elastixImageFilter.Execute()
+
+        R, x, y = elastixImageFilter.GetTransformParameterMap()[0]["TransformParameters"]
+        metric = self.get_metric(logpath)
+
+        # Transfer results back to CPU if needed
+        R = cp.asnumpy(R)
+        x = cp.asnumpy(x)
+        y = cp.asnumpy(y)
+        metric = cp.asnumpy(metric)
+
+        return float(R), float(x), float(y), float(metric)
+
     def align_elastix_with_affine(self, fixed_index, moving_index):
         elastixImageFilter = sitk.ElastixImageFilter()
         fixed_file = os.path.join(self.input, f"{fixed_index}.tif")
@@ -199,6 +308,7 @@ class ElastixManager(FileLogger):
         # rigid = ('0.00157607', '-0.370347', '-3.21588')
         # affine = ('1.01987', '-0.00160559', '-0.000230038', '1.02641', '-1.98157', '-1.88057')
         print(results)
+
 
     def create_affine_transformations(self):
         image_manager = ImageManager(self.input)
