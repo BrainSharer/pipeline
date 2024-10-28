adc5455b6598285ad643506d973332703b30652d
drinehart1 <duane.rinehart@gmail.com>
2024-10-28 13:56:04 -0700

    added additional debug statements to the sections controller

--- src/library/controller/sections_controller.py
+++ src/library/controller/sections_controller.py
@@ -4,38 +4,45 @@ class SectionsController():
     """Class for controlling sections
     """
 
-    def get_sections(self, animal, channel):
+    def get_sections(self, animal: str, channel: int, debug: bool) -> list:
         """The sections table is a view and it is already filtered by active and file_status = 'good'
         The ordering is important. This needs to come from the histology table.
 
         :param animal: the animal to query
         :param channel: 1 or 2 or 3.
+        :param debug: whether to print the raw SQL query
 
         :returns: list of sections in order
-
         """
         slide_orderby = self.histology.side_sectioned_first
         scene_order_by = self.histology.scene_order
+
         if slide_orderby == 'DESC' and scene_order_by == 'DESC':
-            sections = self.session.query(Section).filter(Section.prep_id == animal)\
+            query = self.session.query(Section).filter(Section.prep_id == animal)\
                 .filter(Section.channel == channel)\
                 .order_by(Section.slide_physical_id.desc())\
-                .order_by(Section.scene_number.desc()).all()
+                .order_by(Section.scene_number.desc())
         elif slide_orderby == 'ASC' and scene_order_by == 'ASC':
-            sections = self.session.query(Section).filter(Section.prep_id == animal)\
+            query = self.session.query(Section).filter(Section.prep_id == animal)\
                 .filter(Section.channel == channel)\
                 .order_by(Section.slide_physical_id.asc())\
-                .order_by(Section.scene_number.asc()).all()
+                .order_by(Section.scene_number.asc())
         elif slide_orderby == 'ASC' and scene_order_by == 'DESC':
-            sections = self.session.query(Section).filter(Section.prep_id == animal)\
+            query = self.session.query(Section).filter(Section.prep_id == animal)\
                 .filter(Section.channel == channel)\
                 .order_by(Section.slide_physical_id.asc())\
-                .order_by(Section.scene_number.desc()).all()
+                .order_by(Section.scene_number.desc())
         elif slide_orderby == 'DESC' and scene_order_by == 'ASC':
-            sections = self.session.query(Section).filter(Section.prep_id == animal)\
+            query = self.session.query(Section).filter(Section.prep_id == animal)\
                 .filter(Section.channel == channel)\
                 .order_by(Section.slide_physical_id.desc())\
-                .order_by(Section.scene_number.asc()).all()
+                .order_by(Section.scene_number.asc())
+
+        if debug:
+            # Print the raw SQL query
+            print(f'RAW SQL: {str(query.statement.compile(compile_kwargs={"literal_binds": True}))}')
+
+        sections = query.all()
         return sections
 
 
