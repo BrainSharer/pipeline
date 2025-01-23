100511e7a59d0fb059c6a80ff39d6be25584844a
Edward O'Donnell <eddy.odonnell@gmail.com>
2025-01-23 16:07:42 +0700

    Have to switch to pylibCZIrw for reading from CZI files

--- src/pipeline/scripts/czi_helper.py
+++ src/pipeline/scripts/czi_helper.py
@@ -1,5 +1,8 @@
 import numpy as np
 from aicsimageio import AICSImage
+from aicspylibczi import CziFile
+from pylibCZIrw import czi as pyczi
+
 import os
 import sys
 import argparse
@@ -38,6 +41,51 @@ def run_main(animal, czi_file, scene=0):
                 print(f'{outpath=}')
         print()
 
+def run_mosaic(animal, czi_file, scene=0):
+    inpath = f'/net/birdstore/Active_Atlas_Data/data_root/pipeline_data/{animal}/czi/{czi_file}'
+    czi = CziFile(inpath)
+
+    # Get the shape of the data
+    #dimensions = czi.dims  # 'STCZMYX'
+
+    #czi.get_dims_shape()  # [{'X': (0, 924), 'Y': (0, 624), 'Z': (0, 1), 'C': (0, 1), 'T': (0, 1), 'M': (0, 2), 'S': (0, 1)}]
+    #print(f'dims_shape={czi.get_dims_shape()}')
+
+    #czi.is_mosaic()  # True
+    print(f'is_mosaic={czi.is_mosaic()}')
+    # Mosaic files ignore the S dimension and use an internal mIndex to reconstruct, the scale factor allows one to generate a manageable image
+    scene = czi.get_scene_bounding_box(0)
+    print(f'scenex={scene.x} scene.y={scene.y} scene.w={scene.w} scene.h={scene.h}')
+
+    data = czi.read_mosaic(C=0, scale_factor=1.0, region=(scene.x, scene.y, scene.w, scene.h))
+    print(f'mosaic_data shape={data.shape} data type={data.dtype}')
+    outpath = f'/net/birdstore/Active_Atlas_Data/data_root/pipeline_data/{animal}/preps/C1/test'
+    os.makedirs(outpath, exist_ok=True)
+    outfile = os.path.join(outpath, f'{czi_file}_{str(scene)}_{str(1)}.tif')
+    write_image(outfile, data)
+
+def run_pyczi(animal, czi_file, scene=0):
+    inpath = f'/net/birdstore/Active_Atlas_Data/data_root/pipeline_data/{animal}/czi/{czi_file}'
+    with pyczi.open_czi(inpath) as czidoc:
+        # get the image dimensions as a dictionary, where the key identifies the dimension
+        total_bounding_box = czidoc.total_bounding_box
+        print(f'total_bounding_box={total_bounding_box}')
+
+        # get the total bounding box for all scenes
+        total_bounding_rectangle = czidoc.total_bounding_rectangle
+        print(f'total_bounding_rectangle={total_bounding_rectangle}')
+        # get the bounding boxes for each individual scene
+        scenes_bounding_rectangle = czidoc.scenes_bounding_rectangle
+        print(f'scenes_bounding_rectangle={scenes_bounding_rectangle}')
+        bounding_box = scenes_bounding_rectangle[0]
+        print(f'bounding_box={bounding_box}')
+        data = czidoc.read(plane={"T": 0, "Z": 0, "C": 0}, zoom=1.0, roi=(bounding_box.x, bounding_box.y, bounding_box.w, bounding_box.h))
+        print(f'image2d shape={data.shape} data type={data.dtype} ndim={data.ndim}')
+        outpath = f'/net/birdstore/Active_Atlas_Data/data_root/pipeline_data/{animal}/preps/C1/test'
+        os.makedirs(outpath, exist_ok=True)
+        outfile = os.path.join(outpath, f'{czi_file}_{str(scene)}_{str(1)}.tif')
+        write_image(outfile, data)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Work on Animal")
@@ -51,7 +99,8 @@ if __name__ == "__main__":
     czi = args.czi
     scene = args.scene
 
-    run_main(animal, czi, scene)
-
+    #run_main(animal, czi, scene)
+    #run_mosaic(animal, czi, scene)
+    run_pyczi(animal, czi, scene)
 
 
