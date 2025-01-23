baf57520d778651a3a3f858f1ed0207d001ff9fe
Edward O'Donnell <eddy.odonnell@gmail.com>
2025-01-23 13:02:26 +0700

    Added a write to czi_helper fixed path part 2 set scene

--- src/pipeline/scripts/czi_helper.py
+++ src/pipeline/scripts/czi_helper.py
@@ -18,38 +18,22 @@ def run_main(animal, czi_file):
     czi_file_path = f'/net/birdstore/Active_Atlas_Data/data_root/pipeline_data/{animal}/czi'
     infile = os.path.join(czi_file_path, czi_file)
     czi_aics = AICSImage(infile)
-    #czi = CziFile(infile)
     czi = CZIManager(infile)
 
-    #metadata = czi_aics.metadata  # returns the metadata object for this file format (XML, JSON, etc.)
-    #print(metadata)
-    #for element in metadata.findall('*'):
-    #    for e2 in element.findall('*'):
-    #        print(e2)
-
-
-
-
-    #dimensions = czi.get_dims_shape() 
-    #print(f'dimensions {dimensions}')
-    #dims = czi.dims  # BSCZYX
-    #print(f'dims={dims}')
-    #sizes = czi.size  # (1, 40, 4, 60, 1300, 1900)
-    #print(f'sizes={sizes}')
     total_scenes = czi_aics.scenes
-    scales = [1.0]
+    scales = [1.0, 1/32]
     channels = czi_aics.dims.C
     print(f"{czi_file} has {len(total_scenes)} scenes and {channels} channels")
     for scale in scales:
         for idx, scenei in enumerate(total_scenes):
-            czi_aics.set_scene(scenei)
-            print(f"{scale=} Scene {idx=} {scenei=}", end=" ")
-            data = czi.get_scene(scale=scale, scene_index=idx, channel=1)
-            print(f'shape={data.shape} data type={data.dtype}')
-            if scenei == 0:
+            if idx == scene:
+                czi_aics.set_scene(scenei)
+                print(f"{scale=} Scene {idx=} {scenei=}", end=" ")
+                data = czi.get_scene(scale=scale, scene_index=idx, channel=1)
+                print(f'shape={data.shape} data type={data.dtype}')
                 outpath = f'/net/birdstore/Active_Atlas_Data/data_root/pipeline_data/{animal}/preps/C1/test'
                 os.makedirs(outpath, exist_ok=True)
-                outfile = os.path.join(outpath, f'{czi_file}_{scenei}_{scale}.tif')
+                outfile = os.path.join(outpath, f'{czi_file}_{scenei}_{str(scale)}.tif')
                 write_image(outfile, data)
                 print(f'{outpath=}')
         print()
@@ -59,13 +43,15 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Work on Animal")
     parser.add_argument("--animal", help="Enter the animal", required=True)
     parser.add_argument("--czi", help="Enter the filename", required=True, type=str)
+    parser.add_argument("--scene", help="Enter the scene idx", required=False, default=0, type=int)
 
     args = parser.parse_args()
 
     animal = args.animal
     czi = args.czi
+    scene = args.scene
 
-    run_main(animal, czi)
+    run_main(animal, czi, scene)
 
 
 
