1debd6d2b88b94972c7928316206a440806e6777
Edward O'Donnell <eddy.odonnell@gmail.com>
2025-02-07 12:30:46 +0700

    Switched back to PIL and using PIL to save image and using PIL removed resize

--- src/library/utilities/utilities_registration.py
+++ src/library/utilities/utilities_registration.py
@@ -271,6 +271,8 @@ def align_image_to_affine(file_key):
         print(f'Error={e}')
         sys.exit()
 
+    print(f'Writing {outfile} from matrix {T.flatten()[:6]} with image info={img.info}')
+
     img.save(outfile)
     return
 
