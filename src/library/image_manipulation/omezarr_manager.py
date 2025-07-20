d98f381cb00ce68a9c437a15a0a2291248b710eb
Edward O'Donnell <eddy.odonnell@gmail.com>
2025-07-21 06:40:58 +0700

    Put in test for scratch space back in

--- src/library/image_manipulation/omezarr_manager.py
+++ src/library/image_manipulation/omezarr_manager.py
@@ -189,15 +189,19 @@ class OmeZarrManager():
             input_path = omezarr.transfer_path
             output_path = omezarr.rechunkme_path
             omezarr.write_rechunk_transfer(client, chunks, input_path, output_path)
+            omezarr.cleanup()
 
             # pass 2
             chunks = omezarr.pyramidMap[0]['chunk']
             input_path = omezarr.rechunkme_path
             output_path = os.path.join(omezarr.output, str(0))
             omezarr.write_rechunk_transfer(client, chunks, input_path, output_path)
-            
-            for mip in range(1, len(omezarr.pyramidMap) - 1):
+            omezarr.cleanup()
+        
+            pyramids = len(omezarr.pyramidMap) - 1
+            for mip in range(1, pyramids):
                 omezarr.write_mips(mip, client)
+                omezarr.cleanup()
 
         cluster.close()
-        omezarr.cleanup()
+ 
