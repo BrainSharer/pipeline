e329eb6b6fa5d10634874b11b1686e6bdf415023
Edward O'Donnell <eddy.odonnell@gmail.com>
2025-12-06 07:55:32 +0700

    Changed pixel id to registered image

--- src/library/registration/volume_registration.py
+++ src/library/registration/volume_registration.py
@@ -822,7 +822,7 @@ class VolumeRegistration:
             moving.GetPixelID(),
         )
 
-        resampled = sitk.Cast(sitk.RescaleIntensity(resampled), sitk.sitkUInt8)
+        resampled = sitk.Cast(sitk.RescaleIntensity(resampled), moving.GetPixelID())
         sitk.WriteImage(resampled, self.registered_volume)
         print(f"Resampled moving image written to {self.registered_volume}")
 
